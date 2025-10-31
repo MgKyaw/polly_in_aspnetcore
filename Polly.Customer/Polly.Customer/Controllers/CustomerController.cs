16f6b9915f1f104b99d4d993a3717285f8ff6de3
MgKyaw <mgkyaw20112@gmail.com>
2025-10-31 20:58:26 +0700

    feat: Add new controller

---
+++ Polly.Customer/Polly.Customer/Controllers/CustomerController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Polly.Customer.Controllers;
+public class CustomerController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
