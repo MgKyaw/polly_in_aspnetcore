ca89d381025f6f2b8c1c53e4d2dc0f2038b5a682
MgKyaw <mgkyaw20112@gmail.com>
2025-10-31 20:58:52 +0700

    feat: Update CustomerController

--- Polly.Customer/Polly.Customer/Controllers/CustomerController.cs
+++ Polly.Customer/Polly.Customer/Controllers/CustomerController.cs
@@ -1,10 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Polly.Customer.Controllers;
-public class CustomerController : Controller
+
+[Route("api/[controller]")]
+[ApiController]
+public class CustomerController : ControllerBase
 {
-    public IActionResult Index()
+    private Dictionary<int, string> _customerNameDict = null;
+
+    public CustomerController()
     {
-        return View();
+        if (_customerNameDict == null)
+        {
+            _customerNameDict = new Dictionary<int, string>();
+            _customerNameDict.Add(1, "Pro Code Guide");
+            _customerNameDict.Add(2, "Support - Pro Code Guide");
+            _customerNameDict.Add(3, "Sanjay");
+            _customerNameDict.Add(4, "Sanjay - Pro Code Guide");
+        }
+    }
+
+    [HttpGet]
+    [Route("GetCustomerName/{customerCode}")]
+    public ActionResult<string> GetCustomerName(int customerCode)
+    {
+        if (_customerNameDict != null && _customerNameDict.ContainsKey(customerCode))
+        {
+            return _customerNameDict[customerCode];
+        }
+        return "Customer Not Found";
     }
 }
