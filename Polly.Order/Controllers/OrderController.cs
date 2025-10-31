c0f0711662c12964df68c9eae445437758e0326d
MgKyaw <mgkyaw20112@gmail.com>
2025-10-31 21:21:15 +0700

    feat: Add OrderController

---
+++ Polly.Order/Controllers/OrderController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Polly.Order.Models;
+
+namespace Polly.Order.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class OrderController : ControllerBase
+{
+    private readonly ILogger<OrderController> _logger;
+    private readonly IHttpClientFactory _httpClientFactory;
+    private HttpClient _httpClient;
+    private string apiurl = @"http://localhost:23833/";
+
+    private OrderDetails _orderDetails = null;
+    public OrderController(ILogger<OrderController> logger, IHttpClientFactory httpClientFactory)
+    {
+        _logger = logger;
+        _httpClientFactory = httpClientFactory;
+
+        if (_orderDetails == null)
+        {
+            _orderDetails = new OrderDetails
+            {
+                Id = 7261,
+                SetupDate = DateTime.Now.AddDays(-10),
+                Items = new List<Item>()
+            };
+            _orderDetails.Items.Add(new Item
+            {
+                Id = 6514,
+                Name = ".NET Core Book"
+            });
+        }
+    }
+
+    [HttpGet]
+    [Route("GetOrderByCustomer/{customerCode}")]
+    public OrderDetails GetOrderByCustomer(int customerCode)
+    {
+        _httpClient = _httpClientFactory.CreateClient();
+        _httpClient.BaseAddress = new Uri(apiurl);
+        var uri = "/api/Customer/GetCustomerName/" + customerCode;
+        var result = _httpClient.GetStringAsync(uri).Result;
+
+        _orderDetails.CustomerName = result;
+
+        return _orderDetails;
+    }
+}
