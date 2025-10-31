a73ab7bb2adcad31f3e3d823da579a914ef77133
MgKyaw <mgkyaw20112@gmail.com>
2025-10-31 21:18:37 +0700

    feat: Add OrderDetails

---
+++ Polly.Order/Models/OrderDetails.cs
@@ -0,0 +1,9 @@
+﻿namespace Polly.Order.Models;
+
+public class OrderDetails
+{
+    public int Id { get; set; }
+    public string CustomerName { get; set; }
+    public DateTime SetupDate { get; set; }
+    public List<Item> Items { get; set; }
+}
